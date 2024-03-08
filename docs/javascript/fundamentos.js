d4c25fcf3fb6e60f5a26992df8b8629d0d50e05d
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-03-07 19:50:04 -0500

    javascript

---
+++ docs/javascript/fundamentos.js
@@ -0,0 +1,62 @@
+// Este es un comentario de JavaScript de una sola línea
+/*
+Este es un
+comentario de
+varia líneas
+*/
+
+/* Métodos de interacción */
+// Salida
+/* console.log("mensaje en consola")
+document.write("mensaje en interfaz de usuario")
+alert("mensaje en ventana emergente") */
+
+/* VARIABLES */
+// Declaración
+/* saludo1 = "hola1"
+var saludo2 = "hola2"
+let saludo3 = "hola3"
+const saludo4 = "hola4"
+console.log(saludo1)
+console.log(saludo2)
+console.log(saludo3)
+console.log(saludo4) */
+
+// Ámbito de las variables
+/* let variableGlobal = "soy variable global"
+console.log(variableGlobal)
+{
+  let variableLocal = "soy variable local"
+  console.log(variableLocal)
+} */
+
+/* let cambiaValor = 1;
+const noCambiaValor = 2;
+cambiaValor = "1";
+noCambiaValor = 2;
+console.log(cambiaValor)
+console.log(noCambiaValor) */
+
+/* Modo estricto */
+"use strict"
+/* saludo = "hola";
+console.log(saludo); */
+
+/* Tipos de dato primitivos */
+// string
+let miVariable = "";
+ miVariable = "este es un texto de 'ejemplo 1'";
+ miVariable = 'este es un texto de "ejemplo 2"';
+ miVariable = `este es un 'texto' de "ejemplo" 3`;
+ // number
+ miVariable = 10;
+ miVariable = 9.5;
+ miVariable = 0xFF;
+  miVariable = 123e5;
+// boolean
+ miVariable = true;
+ miVariable = false;
+ console.log(miVariable);
+ // undefined
+ let miVariableSinAsignacion;
+console.log(miVariableSinAsignacion);
